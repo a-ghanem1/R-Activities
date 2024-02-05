b0b1a7234d96af33a25dd29da1afd7246090cc39
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2024-02-05 07:31:54 +0300

    feat: implement pagination and filters

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,5 @@
-using Domain;
 using Application.Activities;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
@@ -8,9 +8,9 @@ namespace API.Controllers;
 public class ActivitiesController : BaseApiController
 {
     [HttpGet]
-    public async Task<IActionResult> GetActivities()
+    public async Task<IActionResult> GetActivities([FromQuery] ActivityParams param)
     {
-        return HandleResult(await Mediator.Send(new List.Query()));
+        return HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
     }
 
     [HttpGet("{id}")]
